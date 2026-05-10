66c67b0074c834e1f2950ff2ad6380040271f7f5
SASA4070 <96961537+SASA4070@users.noreply.github.com>
2026-05-10 10:16:59 +0300

    upload from telegram

---
+++ config.py
@@ -0,0 +1,17 @@
+# ========== التوكن والأدمن ==========
+TOKEN = '8490768092:AAF_GAGrBWUmY4u3NAFATaYwNJDKLjEViLQ'
+ADMIN_ID = 1489001988
+
+# ========== إعدادات الفحص ==========
+RATE_LIMIT_SECONDS = 10
+COMBO_SLEEP_SECONDS = 7
+MAX_CONCURRENT_COMBOS = 5  # أقصى عدد ملفات بتتفحص في نفس الوقت
+
+# ========== إعدادات التوليد ==========
+DEFAULT_EXPIRY_MONTH = "12"
+DEFAULT_EXPIRY_YEAR = "29"
+DEFAULT_CVV = "758"
+
+# ========== إعدادات البروكسي ==========
+USE_PROXY = False  # تشغيل/إيقاف البروكسي
+PROXY_FILE = "proxies/proxies.txt"  # ملف البروكسيات
